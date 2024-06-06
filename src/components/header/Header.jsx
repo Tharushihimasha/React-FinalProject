cee9c558248671106292278b539a5f5101d7b084
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-06 21:46:26 +0530

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,14 +5,14 @@ import Navbar from "./Navbar";
 function Header() {
   return (
     <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1
-     ring-slate-900/5 z-10">
+     ring-slate-900/5 z-10 ">
       <div className="flex gap-x-[400px]  pt-4 pl-4 items-center flex-wrap">
         {/* logo */}
         <div className="">
            <Link><img src={ logo } alt="" width={120} height={70}/></Link>
         </div>
         {/* Navbar */}
-        <Navbar containStyles={"hidden md:flex gap-x-10 "}/>
+        <Navbar containStyles={"hidden md:flex gap-x-10 font-bold "}/>
         {/* button */}
         <div className="">button</div>
       </div>
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,10 +4,10 @@ import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from "react-icons/md"
 function Navbar({containStyles}) {
   return (
     <nav className={`${containStyles}`}>
-      <NavLink to={'/'}><div className="flex items-center gap-x-1"><MdHomeFilled/>Home</div></NavLink>
-      <NavLink to={'/mens'}><div className="flex items-center gap-x-1"><MdCategory/>Men's</div></NavLink>
-      <NavLink to={'/womens'}><div className="flex items-center gap-x-1"><MdShop2/>Women's</div></NavLink>
-      <NavLink to={'/kids'}><div className="flex items-center gap-x-1"><MdContacts/>Kid's</div></NavLink>
+      <NavLink to={'/'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdHomeFilled/>Home</div></NavLink>
+      <NavLink to={'/mens'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdCategory/>Men's</div></NavLink>
+      <NavLink to={'/womens'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdShop2/>Women's</div></NavLink>
+      <NavLink to={'/kids'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdContacts/>Kid's</div></NavLink>
     </nav>
   )
 }
