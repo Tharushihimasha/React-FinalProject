8e8e42ec848db1081d65a37c22b57ada87856512
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-06 21:54:41 +0530

    Update

--- src/components/body/Hero.jsx
+++ src/components/body/Hero.jsx
@@ -5,6 +5,17 @@ function Hero() {
      <section>
         <div>
             <h1>Digital Shopping Hub Juntion</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem alias laudantium tempora porro sapiente repudiandae neque eos pariatur quo ea ab, impedit ducimus. Veniam modi officiis iste voluptatibus quidem aspernatur!
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, eaque sit provident nam harum quis dolorem, alias nulla exercitationem sunt rem sequi, distinctio illum. Aperiam voluptatibus repellendus id asperiores numquam.
+            </p>
+            <div>
+                <div>
+                    <MdStar />
+                    <MdStar />
+                    <MdStar />
+                    <MdStar />
+                </div>
+            </div>
         </div>
      </section>
   )
