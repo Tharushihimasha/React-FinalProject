08b18229841df733a371b2f2ee5e648edb98717d
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-07 12:11:00 +0530

    Update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,9 @@ import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <header className="fixed top-0 left-0 m-auto max_padd_container">
-      <div className="">
+    <header className="fixed top-0 left-0 m-auto max_padd_container w-full bg-white ring-1 ring-slate-900/5
+    z-10">
+      <div className="px flexbetween py-3 max-xs:px-2">
         {/* logo */}
         <div className="">
            <Link><img src={ logo } alt="" width={120} height={70}/></Link>
