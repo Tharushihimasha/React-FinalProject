1783c898c2b787496a9c665b09f3d78fae6272b0
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-07 12:07:38 +0530

    Update

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -4,10 +4,10 @@ import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from "react-icons/md"
 function Navbar({containStyles}) {
   return (
     <nav className={`${containStyles}`}>
-      <NavLink to={'/'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdHomeFilled/>Home</div></NavLink>
-      <NavLink to={'/mens'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdCategory/>Men's</div></NavLink>
-      <NavLink to={'/womens'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdShop2/>Women's</div></NavLink>
-      <NavLink to={'/kids'}><div className="flex items-center gap-x-1 hover:text-yellow-600"><MdContacts/>Kid's</div></NavLink>
+      <NavLink to={'/'}><div className=""><MdHomeFilled/>Home</div></NavLink>
+      <NavLink to={'/mens'}><div className=""><MdCategory/>Men's</div></NavLink>
+      <NavLink to={'/womens'}><div className=""><MdShop2/>Women's</div></NavLink>
+      <NavLink to={'/kids'}><div className=""><MdContacts/>Kid's</div></NavLink>
     </nav>
   )
 }
