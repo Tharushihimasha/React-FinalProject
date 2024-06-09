c5d3bae873fae5ff4fb0dbb55753938df11c416c
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-09 10:58:38 +0530

    Update

--- src/components/body/Hero.jsx
+++ src/components/body/Hero.jsx
@@ -3,10 +3,10 @@ import { MdStar,MdOutlineLocalOffer } from 'react-icons/md'
 
 function Hero() {
   return (
-     <section>
-        <div>
-            <h1>Digital Shopping Hub Juntion</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem alias laudantium tempora porro sapiente repudiandae neque eos pariatur quo ea ab, impedit ducimus. Veniam modi officiis iste voluptatibus quidem aspernatur!
+     <section className=' bg-hero relative bg-center bg-no-repeat h-screen w-full'>
+        <div className='max_padd_container relative top-32 xs:top-52'>
+            <h1 className='h1 capitalize max-w-[37rem]'>Digital Shopping Hub Juntion</h1>
+            <p className='text-gray-50 regular-16 mt-6 max-w-[33rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem alias laudantium tempora porro sapiente repudiandae neque eos pariatur quo ea ab, impedit ducimus. Veniam modi officiis iste voluptatibus quidem aspernatur!
                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, eaque sit provident nam harum quis dolorem, alias nulla exercitationem sunt rem sequi, distinctio illum. Aperiam voluptatibus repellendus id asperiores numquam.
             </p>
             <div>
@@ -26,9 +26,7 @@ function Hero() {
                     <MdOutlineLocalOffer className="text-2xl"/>Offer</div>
             </div>
         </div>
-        <div>
-            
-        </div>
+        
      </section>
   )
 }
