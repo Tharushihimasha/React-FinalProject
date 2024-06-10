4c8eff5d1ffb651034d2a376a8f1a951d5d95b58
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-10 06:48:21 +0530

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import FOOTER_LINKS from '../../assets/data'
+import FOOTER_LINKS from '../../assets/footer_links'
 
 function Footer() {
   return (
@@ -8,14 +8,25 @@ function Footer() {
       <div>
         <div>
           <Link to="/" className='mb-10 bold-20'>Shoppee</Link>
-          <div>
+          <div className='flex flex-wrap gap-8 sm:justify-between md:flex-1'>
             {FOOTER_LINKS.map((col) => (
               <FooterColumn title={col.title} key={col.title}>
-                  <ul>
-                    {col.links.map((link))}
+                  <ul className='flex flex-col gap-4 regular-14 text-gray-20'>
+                    {col.links.map((link) =>(
+                      <Link to="/" key={link}>{link}</Link>
+                    ))}
                   </ul>
               </FooterColumn>
             ))}
+            <div>
+              <FooterColumn title={FOOter_CONTACT_INFO.title}>
+                  {FOOter_CONTACT_INFO.links.map((link) => (
+                    <link to="/" key={link.label}>
+                       <p>{link.label}</p>
+                    </link>
+                  ))}
+              </FooterColumn>
+            </div>
           </div>
         </div>
       </div>
