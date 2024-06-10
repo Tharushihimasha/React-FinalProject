28c342a7f63c04ada1f583c3c9645588b039014b
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-10 05:50:24 +0530

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import FOOTER_LINKS from '../../assets/data'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <div>
+        <div>
+          <Link to="/" className='mb-10 bold-20'>Shoppee</Link>
+          <div>
+            {FOOTER_LINKS.map((col) => (
+              <FooterColumn title={col.title} key={col.title}></FooterColumn>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+};
+
+const FooterColumn = ({title,children}) => {
+  return(
+    <div>
+      <h4>{title}</h4>
+      {children}
+    </div>
   )
 }
 
