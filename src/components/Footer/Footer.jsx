de80e7ac882dd737d8952c8a97f33362a1a3c5fd
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-10 06:03:12 +0530

    Update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,11 @@ function Footer() {
           <Link to="/" className='mb-10 bold-20'>Shoppee</Link>
           <div>
             {FOOTER_LINKS.map((col) => (
-              <FooterColumn title={col.title} key={col.title}></FooterColumn>
+              <FooterColumn title={col.title} key={col.title}>
+                  <ul>
+                    {col.links.map((link))}
+                  </ul>
+              </FooterColumn>
             ))}
           </div>
         </div>
