7156f992b40c73409b8f5ec39ce48100f936a4a4
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-10 08:05:28 +0530

    Update

--- src/components/body/Popular.jsx
+++ src/components/body/Popular.jsx
@@ -11,7 +11,8 @@ function Popular() {
             {/* container */}
             <div>
                 {POPULAR.map((item) => (
-                    <Item  key={item.id} id={item.id}/>
+                    <Item  key={item.id} id={item.id} image={item.image} name={item.name} 
+                    new_price={item_new_price} old_price={item_old_price}/>
                 ))}
             </div>
         </div>
