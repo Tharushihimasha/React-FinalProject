7dbb490be6d3761a1be4cc2ff73af3179765e596
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-10 07:36:42 +0530

    Update

--- src/components/body/Popular.jsx
+++ src/components/body/Popular.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import POPULAR from '../../assets/popular'
 
 function Popular() {
   return (
     <section>
         <div>
             <h3>Popular Products</h3>
+            <hr />
+            {/* container */}
+            <div>
+                {POPULAR.map((item) => (
+                    <Item />
+                ))}
+            </div>
         </div>
     </section>
   )
