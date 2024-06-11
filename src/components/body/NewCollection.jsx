0cbfafee127500b4c789c27c4fd30a3ed25ccc03
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-11 06:58:25 +0530

    Update

--- src/components/body/NewCollection.jsx
+++ src/components/body/NewCollection.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
-import POPULAR from '../../assets/popular'
+import LATEST from '../../assets/latest'
 import Item from './Item'
 
-function Popular() {
+const NewCollection = () => {
   return (
     <section className='bg-primary'>
         <div className='max_padd_container py-12 xl:py-28 xl:w-[88%]'>
@@ -11,7 +10,7 @@ function Popular() {
             to-transparent mb-16' />
             {/* container */}
             <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
-                {POPULAR.map((item) => (
+                {LATEST.map((item) => (
                     <Item  key={item.id} id={item.id} image={item.image} name={item.name} 
                     new_price={item.new_price} old_price={item.old_price}/>
                 ))}
@@ -21,4 +20,4 @@ function Popular() {
   )
 }
 
-export default Popular
+export default NewCollection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/body/Hero'
 import Popular from '../components/body/Popular'
+import Offer from '../components/body/Offer'
+import NewCollection from '../components/body/NewCollection'
 
 function Home() {
   return (
    <>
      <Hero />
      <Popular /> 
+     <Offer />
+     <NewCollection/>
    </>
   )
 }
