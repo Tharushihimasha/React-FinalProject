746c5e566f5c8d8a53ed5ea953a8f7c3677bc829
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-11 09:39:04 +0530

    Update

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -10,13 +10,14 @@ const Category = ({Category,banner}) => {
         <div>
           <img src={banner} alt="" className='block my-7 mx-auto' />
         </div>
-        <div>
-          <h5><span>Showing 1-12</span>Out Of 36 Products</h5>
-          <div>Sort by<MdOutlineKeyboardArrowDown/></div>
+        <div className='flexBetween my-8 mx-2'>
+          <h5><span className='font-bold'>Showing 1-12</span>Out Of 36 Products</h5>
+          <div className='flexBetween max-sm:p-4 gap-x-4 px-8 py-3
+          rounded-5xl ring-1 ring-slate-900/15'>Sort by<MdOutlineKeyboardArrowDown/></div>
         </div>
         <div>
           {all_products.map((item) => {
-            if (Category) {
+            if (Category === item.category) {
               return <Item  key={item.id} id={item.id} image={item.image} name={item.name} 
               new_price={item.new_price} old_price={item.old_price}/>
             }
