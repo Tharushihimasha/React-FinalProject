3e55a3bc2f9c28b3bc6351904f380406aee590c5
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-11 06:27:38 +0530

    Update

--- src/components/body/Popular.jsx
+++ src/components/body/Popular.jsx
@@ -9,7 +9,7 @@ function Popular() {
             <h3>Popular Products</h3>
             <hr />
             {/* container */}
-            <div>
+            <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
                 {POPULAR.map((item) => (
                     <Item  key={item.id} id={item.id} image={item.image} name={item.name} 
                     new_price={item.new_price} old_price={item.old_price}/>
