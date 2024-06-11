99d73a7ff0a46299a9c37b77e162aea3502820af
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-11 05:47:38 +0530

    Update

--- src/components/body/Item.jsx
+++ src/components/body/Item.jsx
@@ -3,14 +3,16 @@ import { Link } from 'react-router-dom'
 
 function Item({id, name, image, old_price, new_price}) {
   return (
-    <div>
-        <div>
-            <Link to={`product/${id}`}></Link>
+    <div className='rounded-xl overflow-hidden shadow-lg'>
+        <div className='relative flexCenter group overflow-hidden translate-all duration-100'>
+            <Link to={`product/${id}`} className='h-12 w-12 bg-white rounded-full
+            flexCenter absolute  top-1/2 bottom-1/2 !py-2 z-20 scale-0 group-hover:scale-100 transition-all duration-700'></Link>
             <img src={image} alt="productImage" />
         </div>
         <div>
             <h4>{name}</h4>
             <div>
+                <div>{new_price}</div>
                 <div>{old_price}</div>
             </div>
         </div>
