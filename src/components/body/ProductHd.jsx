6fb63bfa93e28c7fe18c758269665233d7008aaf
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-11 17:39:13 +0530

    Update

---
+++ src/components/body/ProductHd.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {TbArrowRight} from 'react-icons/tb'
+
+const ProductHd = () => {
+  return (
+    <div>
+      Home <TbArrowRight />
+    </div>
+  )
+}
+
+export default ProductHd
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useContext } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import ProductHd from '../components/body/ProductHd';
 
-function Product() {
+const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products.find((e) => e.id === Number(productId));
   return (
-    <div>Product</div>
+    <section>
+      <div>
+        <ProductHd  product={}/>
+      </div>
+    </section>
   )
 }
 
