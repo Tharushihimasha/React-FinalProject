e3c78182a1e291fc89b2f5b0843eac8d02df1d72
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-11 07:57:08 +0530

    Update

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,8 +1,16 @@
 import { createContext } from "react";
 import all_products from '../assets/all_products'
 
-export const ShopContext = creteContext(null);
+export const ShopContext = createContext(null);
 
-const ShopContextProvider =(props) =>{
+const ShopContextProvider = (props) =>{
     const contextValue = {all_products}
+
+    return(
+        <ShopContext.Provider value={contextValue}>
+           {props.children}
+        </ShopContext.Provider>
+    )
 }
+
+export default ShopContextProvider;
