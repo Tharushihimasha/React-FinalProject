aab5ee15ff2900d385f6c0ee0ce14419c7aed80f
Tharushihimasha <124339603+Tharushihimasha@users.noreply.github.com>
2024-06-12 12:50:05 +0530

    Update

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -6,9 +6,29 @@ const CartItems = () => {
     const {all_products, cartItems, removeFromCart} = useContext(ShopContext);
 
   return (
-    <div>
-      
-    </div>
+    <section>
+        <table>
+            <thead>
+                <tr>
+                    <th className='p-1 py-2'>Products</th>
+                    <th className='p-1 py-2'>Title</th>
+                    <th className='p-1 py-2'>Price</th>
+                    <th className='p-1 py-2'>Quantity</th>
+                    <th className='p-1 py-2'>Total</th>
+                    <th className='p-1 py-2'>Remove</th>
+                </tr>
+            </thead>
+            <tbody>
+                {all_products.map((e) =>{
+                    if(cartItems[e.id] > 0){
+                        return <tr> key={e.id}
+                            <td><img src={e.image} alt="" /></td>
+                        </tr>
+                    }
+                })}
+            </tbody>
+        </table>
+    </section>
   )
 }
 
